fde1262cf079a075cac797b1850587de41de22ae
Matheus Kops Guedes <matheuskopsguedes@gmail.com>
2025-01-21 01:04:14 +0100

    ça fonctionne pas j'en peux plus

--- tp poudlard/poudlard.pl
+++ tp poudlard/poudlard.pl
@@ -8,7 +8,7 @@ declaration(drago, (coupable(drago) -> innocent(neville))).
 declaration(neville, (coupable(drago) -> innocent(ginny))).
 declaration(ginny, (coupable(ginny) -> (innocent(drago), innocent(neville)))).
 
-% Une personne est innocente si elle n'est pas coupable
+% Une personne est innocente si elle n est pas coupable
 innocent(X) :- \+ coupable(X).
 
 % Règles pour déterminer qui est coupable
@@ -16,7 +16,7 @@ coupable(drago) :- \+ innocent(drago), innocent(neville), innocent(ginny).
 coupable(neville) :- \+ innocent(neville), innocent(drago), innocent(ginny).
 coupable(ginny) :- \+ innocent(ginny), innocent(drago), innocent(neville).
 
-% Définition de la règle pour déterminer qu'une seule personne est coupable parmi les suspects
+% Définition de la règle pour déterminer qu une seule personne est coupable parmi les suspects
 unique_coupable([X, Y, Z]) :-
     coupable(X), innocent(Y), innocent(Z);
     innocent(X), coupable(Y), innocent(Z);
